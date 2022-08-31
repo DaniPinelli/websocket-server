fcaca2bbee748d9cc269a038de1d1771361ec0f8
DaniPinelli <danielpinelli.fullstack@gmail.com>
2022-08-31 17:56:31 -0300

    Sockets working

---
+++ models/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+
+        this.paths = {}
+
+        //Middlewares
+        this.middlewares();
+
+        //Routes
+        this.routes();
+
+        //Sockets
+        this.sockets();
+    }
+
+    middlewares() {
+
+        //Cors
+        this.app.use(cors());
+
+        //Public folder
+        this.app.use(express.static('public'));
+
+        //Fileupload
+        // this.app.use(fileUpload({
+        //     useTempFiles : true,
+        //     tempFileDir : '/tmp/',
+        //     createParentPath: true
+        // }));
+    }
+
+    routes() {}
+
+    sockets(){
+        this.io.on('connection', socket  => {
+            console.log('Socket connected');
+    });
+    }
+
+    listen() {
+        this.server.listen(this.port, () => console.log('Server started on port', this.port));
+    }
+
+}
+
+
+module.exports = Server;
